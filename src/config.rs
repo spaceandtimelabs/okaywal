b9c09177307a71105e4c5e101c93f4793ff9f7f9
David Alves <david.alves@dfinity.org>
2023-11-03 08:41:03 -0700

    Truncate files on startup
    When the checkpoint thread fails, we accumulate a lot of files
    but the startup process doesn't trim them. This adds a max_inactive_files
    configuration and enforces it on startup.
    
    TODO: Finish unit test

--- src/config.rs
+++ src/config.rs
@@ -32,6 +32,9 @@ pub struct Configuration<M> {
     /// was to allow detection of what format or version of a format the data
     /// was inside of the log without needing to parse the entries.
     pub version_info: Arc<Vec<u8>>,
+    /// The max number of inactive files to keep around.
+    /// This is a soft limit.
+    pub max_inactive_files: u32,
 }
 
 impl Default for Configuration<StdFileManager> {
@@ -72,6 +75,7 @@ where
             checkpoint_after_bytes: kilobytes(768),
             buffer_bytes: kilobytes(16),
             version_info: Arc::default(),
+            max_inactive_files: 10,
         }
     }
     /// Sets the number of bytes to preallocate for each segment file. Returns `self`.
@@ -102,6 +106,13 @@ where
     /// writing data to the log. Returns `self`.
     pub fn buffer_bytes(mut self, bytes: usize) -> Self {
         self.buffer_bytes = bytes;
+        self
+    }
+
+    /// Sets the maximum number of inactive files to keep around.
+    /// Returns 'self'.
+    pub fn max_inactive_files(mut self, max_inactive_files: u32) -> Self {
+        self.max_inactive_files = max_inactive_files;
         self
     }
 
